44a80ce6cae0f1c8fc4f47758d7a13f90bf43b0f
myra1234 <34545961+myra1234@users.noreply.github.com>
2021-08-11 01:13:49 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,81 @@
+The Go Playground
+ Imports
+
+Hello, playground
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		for {
+			c1 <- "Every 500 ms"
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
+
+	go func() {
+		for {
+			c2 <- "Every 2 seconds"
+			time.Sleep(time.Second * 2)
+		}
+	}()
+
+	for {
+		select {
+		case msg1 := <-c1:
+			fmt.Println(msg1)
+
+		case msg2 := <-c2:
+			fmt.Println(msg2)
+		}
+	}
+}
+
